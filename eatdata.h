20d411cf2d3cfdad53364c877df8aaea50014d18
subjects inadequate <48841453+inadequatesubject@users.noreply.github.com>
2019-03-22 21:41:28 +0100

    Create eatdata.h
    manages secure file state - overwrite security of file

---
+++ eatdata.h
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <string.h>
+
+class SecureObject {
+
+private:
+  int secure_number;  // number of security index
+  void scramble();  // scramble with algo
+  char string[2048];  // string to scramble
+  
+public:
+  void initr() {
+    for (int i=0; i<=2047; i++) {
+      // print rotating star
+      this->string[i] = '\0';
+    }
+    printf("INITIALIZED\n\n");
+  }
+
+  void setNumber() {
+  
+  }
+  
+  int getNumber() {
+  
+  }
+
+};
