5e9ba25a5e695aff2967e49368a1472b20b5527c
subjects inadequate <48841453+inadequatesubject@users.noreply.github.com>
2019-03-22 21:56:00 +0100

    Dev branch (#3)
    * Create eatdata.h
    
    manages secure file state - overwrite security of file
    
    * Update eatdata.h
    
    * Update eatdata.h

---
+++ eatdata.h
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <string.h>
+
+class SecureObject {
+
+private:
+  int secure_number;  // number of security index
+  char priv_string[2048];  // string to scramble
+  
+public:
+  
+  void acquire_init_string_from_user() {  // acquire string from user to feed class
+    char buffer[2048];
+    
+    printf("FEED: ");
+    scanf("%s", buffer);
+    
+    printf("\nACQUIRED: %s\n\n", buffer);
+    strncpy(buffer,2048,((this)->priv_string));
+    
+    // debugging output
+    printf("1: %s\n2: %s\n\n",buffer,((this)->priv_string));
+  }
+  
+  void setNumber() {
+    // sets number
+  }
+  
+  void initr() {
+    for (int i=0; i<=2047; i++) {
+      // print rotating star
+      this->string[i] = '\0';
+    }
+    // acquire() string
+    (this)->acquire_init_string_from_user();
+    (this)->setNumber(100);
+    printf("INITIALIZED\n\n");
+  }
+
+  int getNumber() {
+    // gets number
+  }
+  
+  char getPrivstring() {
+    // gets priv_string 
+  }
+  
+  void scramble() {
+    // seed random number generator first
+  }  // scramble string with algo
+
+};
