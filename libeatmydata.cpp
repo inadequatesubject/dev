0eecbda476a405be04677c3a9960fab8bd960979
subjects inadequate <48841453+inadequatesubject@users.noreply.github.com>
2019-03-23 13:01:50 +0100

    Create libeatmydata.cpp

---
+++ libeatmydata.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+class Beast {
+
+private:
+  int count;
+  char short_array[10][2048];
+  char BEAST[100][2048];
+  char shadowbeast[100][2048]
+  
+  int repeat;
+  
+public:
+  
+  int sort_array(char a[], int n1=0);
+
+  void ifInitialized() {
+    count++;
+    this->repeat-=1;
+    sort_array((this)->BEAST, -1); // thread()
+  }
+};
+
+int BEAST::sort_array(char a[], int n1) {
+  for (int i=0; this->repeat-- > 0; i++) {
+    if (n1 == -1)
+      break;
+  }
+};
+
+void initr() {
+  count=0;
+  repeat=100;
+}
